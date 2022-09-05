e1eb8dd65138cf9761cff1e770520ffbbe99239d
VismayaSathyan <vismayasathyan157@gmail.com>
2022-09-06 00:21:43 +0530

    Prompt7

---
+++ JavaPromptGIT/src/com/vismaya/Palindrome.java
@@ -0,0 +1,20 @@
+//7. To find out whether the given String is Palindrome or not.
+package com.vismaya;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a Word: ");
+        String word = in.nextLine();
+        String revWord = "";
+        char ch;
+        for (int i=0; i<word.length(); i++){
+            ch = word.charAt(i);
+            revWord = ch + revWord;
+        }
+        String result = revWord.equals(word) ? "Is Palindrome": "Not a Palindrome";
+        System.out.println(result);
+    }
+}
