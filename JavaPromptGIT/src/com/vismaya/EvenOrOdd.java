812ca2cbc8d25b802898989fb3c074f6359fe2b5
VismayaSathyan <vismayasathyan157@gmail.com>
2022-09-05 22:31:08 +0530

    PromptOne

---
+++ JavaPromptGIT/src/com/vismaya/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.vismaya;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = in.nextInt();
+//        if(number % 2 == 0)
+//            System.out.println("Even");
+//        else
+//        System.out.println("Odd");
+        String result = (number % 2) == 0 ? "Even":"Odd";
+        System.out.println(result);
+    }
+}
