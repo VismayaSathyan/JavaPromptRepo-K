e3434dab4aa2c3ba7dabec17f940c63cf15f9a49
VismayaSathyan <vismayasathyan157@gmail.com>
2022-09-23 21:05:46 +0530

    LinearUpdate1

--- JavaPromptGIT/src/com/vismaya/ArraySearch/LinearSearchMain.java
+++ JavaPromptGIT/src/com/vismaya/ArraySearch/LinearSearchMain.java
@@ -3,24 +3,47 @@ package com.vismaya.ArraySearch;
 public class LinearSearchMain {
     public static void main(String[] args) {
         int[] arr = {1,5,7,9,0,-1, 2, 5};
-        int item  = 1000;
-         linearSearch(arr, item);
-        //System.out.println(ans);
+        int item1  = 2;
+        int item2 = 7;
+        int start = 1;
+        int end = 5;
+        linearSearch(arr, item1);
+        System.out.println(" ");
+        System.out.println("In Range " +start+ " to " +end+ " Search");
+        indexRangeLinearSearch(start,end, arr, item2);
     }
-    static boolean linearSearch(int[] arr, int item){
+    static void linearSearch(int[] arr, int item){
 
         if(arr.length == 0){
             System.out.print("The array is empty");
-            return false;
+            //return false;
         }
-        for(int i=0; i<arr.length; i++) {
-            int element = arr[i];
+        for(int element : arr) {
+            //int element = arr[i];
             if (element == item) {
-                System.out.println("item is found at index: " +i);
-                return true;
+                System.out.println("item is found : " +element);
+                //return true;
             }
         }
         System.out.println("Item is not found");
-        return false;
+        //return false;
+    }
+    static void indexRangeLinearSearch(int start, int end, int[] arr, int item){
+        if(arr.length == 0){
+            System.out.print("The array is empty");
+            //return false;
+        }
+        for(int i = start; i<=end; i++){
+            //for(int element : arr){
+            int element = arr[i];
+                if(element == item){
+                    System.out.println("Item is found :" +element);
+                    System.out.println("item is found at index: " +i);
+                    //return true;
+                }
+            //}
+        }
+        System.out.println("Item is not found");
+       // return false;
     }
 }
